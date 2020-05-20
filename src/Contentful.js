37f829608842f21a1c74802f61d7fec2961547ba
CHLCN <chaihailun2012@126.com>
2020-05-20 12:31:05 +0800

    添加图片

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,8 +1,8 @@
 import {createClient} from 'contentful'
 
 export default createClient({
-  // space:process.env.REACT_APP_API_SPACE,
-  // accessToken:process.env.REACT_APP_ACCESS_TOKEN
-  accessToken: 'myAccessToken',
-  space: 'mySpaceId'
+  space:process.env.REACT_APP_API_SPACE,
+  accessToken:process.env.REACT_APP_ACCESS_TOKEN
+  // accessToken: 'myAccessToken',
+  // space: 'mySpaceId'
 })
